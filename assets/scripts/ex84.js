2ecc1805048cf7647d27674689d42ccb7f734dd0
Kiven Alaric <elmozaro3@gmail.com>
2022-12-01 09:44:59 -0600

    addition of new js files

--- assets/scripts/ex84.js
+++ assets/scripts/ex84.js
@@ -1,8 +1,25 @@
 function encodeString(word) {
-    let result = word.;
+    let result = btoa(word);
     return result;
 }
 
 const word2 = 'Kimberly'
 console.log(word2)
 console.log(encodeString(word2))
+
+//btoa stands for binary to array.
+//it creates a base64 encoded ASCII string
+//btoa()= binary to ASCII
+
+//atob stands for array to binary
+//it decodes a base64 encoded string
+
+
+function decodeString(word3) {
+    let result = atob(word3);
+    return result;
+}
+
+const word4 = 'S2ltYmVybHk='
+console.log(word4)
+console.log(decodeString(word4))
