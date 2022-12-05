37994ffc4d58146d1fe9c1dd98ac192eab6225bb
Kiven Alaric <elmozaro3@gmail.com>
2022-12-05 10:50:34 -0600

    addition of js files

--- assets/scripts/ranker.js
+++ assets/scripts/ranker.js
@@ -1,21 +1,38 @@
 function compareTriplets(arr) {
-  let x = arr.length - 1,
-    m = 0;
-  let sum1 = 0,
-    sum2 = 0;
-  for (let y = 1; y < arr.length; y++) {
-    sum2 += arr[y][m];
-    m++;
-  }
-
-  for (let y = 1; y < arr.length; y++) {
-    sum1 += arr[y][x - 1];
-    x--;
-  }
-  let absoluteDifference = Math.abs(sum2 - sum1);
-  console.log(absoluteDifference);
+    let x=arr.length-1, m=0;
+    let sum1=0, sum2 =0;
+    for(y=0; y<arr.length; y++){
+        console.log(arr[y][m]);
+        sum1 += arr[y][m];
+        m++
+    }
+    console.log(sum2);
+    for(y=0; y<arr.length; y++){
+        console.log(arr[y][x]);
+        sum2 += arr[y][x];
+        x--
+    }
+    console.log(sum1);
+    let absoluteDifference= Math.abs(sum1 - sum2) 
+    console.log(absoluteDifference)
 }
 
-let arr1 = [[3], [11, 2, 4], [4, 5, 6], [10, 8, -12]];
+let arr1 =[[11,2,4],[4,5,6],[10, 8, -12]]
 
-compareTriplets(arr1);
+compareTriplets(arr1)
+
+// function diagonalDifference(arr) {
+//     // Write your code here
+//     let sum1=0, sum2 =0;
+//     for(let x=0; x < arr.length; x++){
+//         for(let y=0; y<arr.length; y++){
+//         if(x == y){
+//             sum1 += arr[x][y]
+//         }
+//         if((x+y) == (arr.length-1)){
+//             sum2 += arr[x][y]
+//         } 
+//     }
+//     }
+//     return Math.abs(sum1-sum2)
+// }
